9f4441673b5d4fb6f637913845fb27714989d218
Dizan Vasquez <dichodaemon@gmail.com>
2009-08-26 06:47:39 +0000

    * Added src directory.

---
+++ smart-monitor/src/smart-monitor/main.cpp
@@ -0,0 +1,15 @@
+#include <smart-interfaces/Messages.h>
+#include <rotor/Rotor.h>
+#include <rotor/Structure.h>
+#include <rotor/Registry.h>
+
+
+//------------------------------------------------------------------------------
+
+int
+main( int argc, char * argv[] )
+{
+
+  Registry registry = 
+  
+}
