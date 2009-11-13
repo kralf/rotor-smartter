b9535d66759ce5a5ce9441d59093866103a05585
Ralf Kaestner <ralf.kaestner@gmail.com>
2009-11-13 11:48:14 +0000

    Added smart_init_odometrypos_msg interface Added command-line switch to smartCarmen that overrides the enabled flag for velocity/steering control

--- smart-rotor-interfaces/src/smart-rotor-interfaces/Messages.h
+++ smart-rotor-interfaces/src/smart-rotor-interfaces/Messages.h
@@ -18,7 +18,7 @@ ROTOR_DEFINE_TYPE( axt_message,
 
     int8_t * channel;
     int8_t * point_status;
-    
+
     float *x;
     float *y;
     float *z;
@@ -60,7 +60,7 @@ ROTOR_DEFINE_TYPE( carmen_base_velocity_message,
 ROTOR_DEFINE_TYPE( carmen_laser_laser_config_t,
   struct carmen_laser_laser_config_t {
     int32_t  laser_type;
-    double start_angle;                   
+    double start_angle;
     double fov;
     double angular_resolution;
     double maximum_range;
@@ -86,20 +86,20 @@ ROTOR_DEFINE_TYPE( carmen_robot_laser_message,
   struct carmen_robot_laser_message {
     int32_t id;
     carmen_laser_laser_config_t config;
-    int32_t num_readings;                  
-    float * range;                      
-    int8_t * tooclose;                    
-    int32_t num_remissions;                
-    float * remission;                  
-    carmen_point_t laser_pose;         
-    carmen_point_t robot_pose;         
+    int32_t num_readings;
+    float * range;
+    int8_t * tooclose;
+    int32_t num_remissions;
+    float * remission;
+    carmen_point_t laser_pose;
+    carmen_point_t robot_pose;
     double tv;
-    double rv;                     
+    double rv;
     double forward_safety_dist;
     double side_safety_dist;
     double turn_axis;
-    double timestamp;                    
-    char *host;                          
+    double timestamp;
+    char *host;
   };
 )
 
@@ -127,25 +127,33 @@ ROTOR_DEFINE_TYPE( path_message,
   };
 )
 
-ROTOR_DEFINE_TYPE( smart_velocity_message,
-  struct smart_velocity_message {
+ROTOR_DEFINE_TYPE( smart_status_message,
+  struct smart_status_message {
+    double gas_pos;
+    uint32_t gear;
+    double steering_angle;
     double tv;
-    double steering_angle; 
+    double rv_front_right;
+    double rv_front_left;
+    double rv_rear_right;
+    double rv_rear_left;
     double timestamp;
     char* host;
   };
 )
 
-ROTOR_DEFINE_TYPE( smart_status_message,
-  struct smart_status_message {
-    double gas_pos;
-    uint32_t gear;      
-    double steering_angle;
+ROTOR_DEFINE_TYPE( smart_init_odometrypos_message,
+  struct smart_init_odometrypos_message {
+    carmen_point_t odometrypos;
+    double timestamp;
+    char *host;
+  };
+)
+
+ROTOR_DEFINE_TYPE( smart_velocity_message,
+  struct smart_velocity_message {
     double tv;
-    double rv_front_right;
-    double rv_front_left; 
-    double rv_rear_right; 
-    double rv_rear_left;  
+    double steering_angle;
     double timestamp;
     char* host;
   };
@@ -163,6 +171,14 @@ ROTOR_DEFINE_TYPE( locfilter_filteredpos_message,
   struct locfilter_filteredpos_message {
     carmen_point_t filteredpos;
     carmen_point_t odometrypos;
+    double timestamp;
+    char *host;
+  };
+)
+
+ROTOR_DEFINE_TYPE( locfilter_init_filteredpos_message,
+  struct locfilter_init_filteredpos_message {
+    carmen_point_t filteredpos;
     double timestamp;
     char *host;
   };
